aec0c30939ebf2980cd2d7cdfadcd6f140357af1
Jihye <jihye@Jihyeui-MacBookPro.local>
2020-12-29 15:54:12 +0900

    naver1

---
+++ src/App copy.vue
@@ -0,0 +1,151 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <!-- Must have the app property -->
+      <v-card class="mx-auto" width="256" tile>
+        <v-navigation-drawer v-model="drawer" app>
+          <v-system-bar></v-system-bar>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img>
+                  <img src="@/assets/img-me.jpg" alt="" />
+                </v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title class="title">
+                  퍼블리싱 연습장
+                </v-list-item-title>
+                <v-list-item-subtitle>jihes109@naver.com</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-icon>mdi-menu-down</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+          <v-divider></v-divider>
+          <v-list nav dense>
+            <v-list-item-group v-model="selectedItem" color="primary">
+              <v-list-item v-for="(item, i) in items" :key="i">
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+      </v-card>
+
+      <v-app-bar app>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Application</v-toolbar-title>
+      </v-app-bar>
+
+      <v-main>
+        <div class="section-guide">
+          <h1>NAVER</h1>
+          <h2>그리드와 컴포넌트 요소</h2>
+          <v-container class="top-search">
+            <v-img class="">
+              <img class="main-logo" src="@/assets/logo-naver.png" alt="" />
+            </v-img>
+            <v-text-field
+              v-model="message2"
+              solo
+              label="Solo"
+              clearable
+            ></v-text-field>
+            <v-btn tile color="success">
+              <v-icon center>
+                mdi-magnify
+              </v-icon>
+            </v-btn>
+          </v-container>
+          <v-container fluid>
+            <v-row>
+              <v-card>
+                <v-tabs
+                  success
+                  background-color="white "
+                  color="success"
+                  show-arrows
+                >
+                  <v-tabs-slider color="teal lighten-3"></v-tabs-slider>
+
+                  <v-tab v-for="(tablist, i) in tablist" :key="i">
+                    <v-list-item-title
+                      v-text="tablist.text"
+                    ></v-list-item-title>
+                  </v-tab>
+                </v-tabs>
+              </v-card>
+            </v-row>
+            <v-row>
+              <v-col cols="12" sm="6">
+                2
+              </v-col>
+              <v-col cols="12" sm="6">
+                3
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+// 좌측네비게이션 하단 리스트
+export default {
+  data: () => ({
+    dialog: false,
+    drawer: null,
+    items: [
+      { text: "네이버", icon: "mdi-account-outline" },
+      { text: "토스", icon: "mdi-bike" },
+      { text: "page", icon: "mdi-package-variant" },
+      { text: "page", icon: "mdi-comment-processing-outline" },
+      // { text: "Offline", icon: "mdi-check-circle" },
+      // { text: "Uploads", icon: "mdi-upload" },
+      // { text: "Backups", icon: "mdi-cloud-upload" },
+    ],
+    tablist: [
+      { text: "메일" },
+      { text: "카페" },
+      { text: "블로그" },
+      { text: "지식IN" },
+      { text: "쇼핑" },
+      { text: "PAY" },
+      { text: "TV" },
+      { text: "사전" },
+      { text: "뉴스" },
+      { text: "증권" },
+      { text: "부동산" },
+      { text: "지도" },
+      { text: "영화" },
+    ],
+  }),
+};
+</script>
+
+<style scoped>
+@import "./assets/css/common.css";
+.section-guide {
+  padding: 20px;
+}
+.section-guide > h1,
+.section-guide > h2 {
+  padding-left: 10px;
+  border-bottom: 1px solid #ddd;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,225 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <body>
+    <section class="form-center ">
+      <div class="top-left ">
+        <button class="button-s-grey "><h4>네이버를 시작페이지로</h4></button>
+        <button class="button-s-grey"><h4>쥬니어네이버</h4></button>
+        <button class="button-s-grey"><h4>해피빈</h4></button>
+      </div>
+      <div class="top-search">
+        <img src="@/assets/logo-naver.png" class="logo-default" alt="" />
+        <form class="search-input">
+          <input type="text" class="inputbox" id="searchkey" name="searchkey" />
+          <button class="btn-submit"><v-icon>mdi-magnify</v-icon></button>
+        </form>
+      </div>
+    </section>
+    <div class="nav-bg">
+      <section class="group-nav ">
+        <div class="group-nav-left">
+          <ul class="list-nav">
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">메일</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">카페</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">블로그</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">지식IN</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">쇼핑</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">PAY</h3></a>
+            </li>
+            <li class="nav-item a-green">
+              <a href=""><h3 class="primary">TV</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>사전</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>뉴스</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>부동산</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>지도</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>영화</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>VIBE</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>책</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""><h3>웹툰</h3></a>
+            </li>
+            <li class="nav-item a-none">
+              <a href=""
+                ><h3>더보기 <v-icon>mdi-menu-down</v-icon></h3>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="group-nav-right a-none">
+          <a href=""
+            ><h4>
+              검색어 필터설정이 필요합니다 <v-icon>mdi-chevron-down</v-icon>
+            </h4></a
+          >
+        </div>
+      </section>
     </div>
-    <router-view />
-  </div>
+    <section class="content ">
+      <div class="content-left">
+        <div class="imgbanner">
+          <img src="@/assets/img-main-banner.png" class="width100" alt="" />
+        </div>
+        <div class="news">
+          <div class="newsheader">
+            <div class="newsheader-left">
+              <a href="" class="margin-r black">연합뉴스 </a>
+              <a href="" class=" black"
+                ><h4>
+                  사흘 만에 또 1천명대…영국발 변이 바이러스까지 엎친데 덮친격
+                </h4></a
+              >
+            </div>
+            <div class="newsheader-right">
+              <a href="" class="margin-r blue border"><h4>네이버뉴스</h4></a>
+              <a href="" class="margin-r black border"><h4>연예</h4></a>
+              <a href="" class="margin-r black border "><h4>스포츠</h4></a>
+              <a href="" class="black border "><h4>경제</h4></a>
+            </div>
+          </div>
+          <div class="newsmiddle">
+            <div class="newsmiddle-left">
+              <a href="" class="margin-r black"
+                ><v-icon>mdi-format-list-bulleted</v-icon>뉴스스탠드
+              </a>
+              <a href="" class=" margin-r black">
+                구독한언론사
+              </a>
+              <a href="" class=" black border">
+                전체언론사
+              </a>
+            </div>
+            <div class="newsheader-right">
+              <a href="" class="margin-r  border"
+                ><v-icon>mdi-format-list-bulleted</v-icon></a
+              >
+              <a href="" class="margin-r border"
+                ><v-icon>mdi-view-module</v-icon></a
+              >
+              <a href="" class=" border"><v-icon>mdi-view-module</v-icon></a>
+            </div>
+          </div>
+        </div>
+        <div class="newslist">
+          <div class="newslist-row">
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+          </div>
+          <div class="newslist-row">
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+          </div>
+          <div class="newslist-row">
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+          </div>
+          <div class="newslist-row">
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+            <v-col cols="12" sm="2" class="newscard">
+              <a href="">뉴스</a>
+            </v-col>
+          </div>
+        </div>
+      </div>
+      <div class="content-right"></div>
+    </section>
+  </body>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style scoped>
+@import "./assets/css/common.css";
+.section-guide {
+  padding: 20px;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.section-guide > h1,
+.section-guide > h2 {
+  padding-left: 10px;
+  border-bottom: 1px solid #ddd;
 }
 </style>
